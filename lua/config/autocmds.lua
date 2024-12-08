228113a77200325fa9b3a82cf3347015658e2739
LingLambda <abc1514671906@163.com>
2024-12-08 21:49:52 +0800

    init

---
+++ lua/config/autocmds.lua
@@ -0,0 +1,15 @@
+-- Autocmds are automatically loaded on the VeryLazy event
+-- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
+-- Add any additional autocmds here
+vim.api.nvim_create_augroup("lazyvim_wrap_spell", { clear = true })
+
+local lspconfig = require("lspconfig")
+
+lspconfig.volar.setup({
+  filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
+  init_options = {
+    vue = {
+      hybridMode = false,
+    },
+  },
+})
